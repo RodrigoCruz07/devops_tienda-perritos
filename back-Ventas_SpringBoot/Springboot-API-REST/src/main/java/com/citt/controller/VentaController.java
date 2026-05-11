806eab5f4690e798de1b640193f9514b6e857159
RodrigoCruz07 <rodr.cruz@duocuc.cl>
2026-05-11 08:42:13 -0400

    Fix formatting in VentaController operation summary

--- back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/controller/VentaController.java
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/java/com/citt/controller/VentaController.java
@@ -23,7 +23,7 @@ public class VentaController {
     @Autowired
     private VentaService ventaService;
 
-    @Operation(summary = "Crear una nueva venta", description = "Crea una nueva venta en el sistema")
+    @Operation(summary = "Crear una nueva venta ", description = "Crea una nueva venta en el sistema")
     @PostMapping
     public ResponseEntity<Venta> crearVenta(@Valid @RequestBody Venta venta){
         URI location = ServletUriComponentsBuilder
